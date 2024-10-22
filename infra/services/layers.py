0f0e9d2ff06329a78ae34019c2fd470c6390f2fd
Guilherme Alves pimenta <guialvespimenta27@gmail.com>
2024-10-22 00:55:52 -0300

    changing ecr public repo

--- infra/services/layers.py
+++ infra/services/layers.py
@@ -19,14 +19,6 @@ class Layers:
             layer_version_arn="arn:aws:lambda:us-east-2:211125768252:layer:robots:1",
         )
 
-        self.trello_layer = _lambda.LayerVersion(
-            scope,
-            id="TrelloLayer",
-            code=_lambda.Code.from_asset(Path.layer("layers/trello")),
-            compatible_runtimes=[_lambda.Runtime.PYTHON_3_9],
-            description="",
-        )
-
         self.clickup_layer = _lambda.LayerVersion(
             scope,
             id="ClickupLayer",

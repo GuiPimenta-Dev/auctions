2549846bfc0be1160185dc4209f5b920d8f3377b
Guilherme Alves pimenta <guialvespimenta27@gmail.com>
2024-10-31 09:31:42 -0300

    adding logo to form

--- functions/robots/leiloes_imovel/config.py
+++ functions/robots/leiloes_imovel/config.py
@@ -15,7 +15,7 @@ class LeiloesImovelConfig:
         services.secrets_manager.google_sheets_secret.grant_read(function)
 
         services.event_bridge.schedule(
-            expression="cron(0 8 * * ? *)",
+            expression="cron(0 10 * * ? *)",
             rule_name="auctions",
             function=function,
         )
